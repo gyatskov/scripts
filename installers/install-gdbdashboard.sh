738968f7d281a319e8d40bb8428515e55c317cbc
Gennadij Yatskov <vokstay@gmx.de>
2020-10-15 16:15:14 +0200

    [installers] Add gdbdashboard

---
+++ installers/install-gdbdashboard.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+##
+## @author Gennadij Yatskov (gennadij@yatskov.de)
+##
+## Downloads .gdbinit to home directory
+##
+## Requirements:
+##  * wget
+
+wget -P ~ https://git.io/.gdbinit
