c89ef3c45e7dadb95bd315f279f6b6ddee68df6e
Gennadij Yatskov <vokstay@gmx.de>
2021-02-09 11:52:56 +0100

    [jenkins] Improve get-progressive-console-log.sh
     * Make executable
     * Add usage

--- jenkins/get-progressive-console-log.sh
+++ jenkins/get-progressive-console-log.sh
@@ -13,16 +13,33 @@ readonly SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 # Load configuration
 source <(grep = $SCRIPTPATH/config.ini)
 
+function usage()
+{
+    echo "Usage: $0 <jenkins-job-name> <jenkins-job-id> [line-start=0]"
+    echo ""
+    echo "Examples:"
+    echo "Get progressive log in pipeline 'code-review' with id 123456 from line 500:"
+    echo "$0 code-review 123456 500"
+    echo ""
+    echo "Get progressive log in pipeline 'code-review' with id 123456 completely:"
+    echo "$0 code-review 123456"
+}
+
+if [[ $# -lt 2 ]]; then
+    usage
+    exit 1
+fi
+
 readonly JOB_NAME="$1"
 readonly JOB_ID="$2"
+readonly LINE_START="${3:-0}"
 
 # Progressive log output
 function progressive_log()
 {
     local -r _line_start=$1
     curl -u "${JENKINS_USER}:${JENKINS_API_TOKEN}" \
-        "$JENKINS_URL/job/$JOB_NAME/${JOB_ID}/logText/progressiveText?start=$_line_start"
+        "$JENKINS_URL/job/$JOB_NAME/${JOB_ID}/logText/progressiveText?start=${_line_start}"
 }
 
-readonly line_start=$1
-progressive_log $line_start
+progressive_log $LINE_START
