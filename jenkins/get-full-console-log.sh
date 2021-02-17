89be690d98e7e0f565700e0126aaf89ce3ac1d1d
Gennadij Yatskov <vokstay@gmx.de>
2021-02-17 15:54:59 +0100

    [jenkins] Document get-full-console-log
    Provide usage.

--- jenkins/get-full-console-log.sh
+++ jenkins/get-full-console-log.sh
@@ -12,6 +12,21 @@ readonly SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 # Load configuration
 source <(grep = $SCRIPTPATH/config.ini)
 
+function usage()
+{
+    echo "Usage: $0 <jenkins-job-name> <jenkins-job-id>"
+    echo ""
+    echo "Examples:"
+    echo "Get full console log in pipeline 'code-review' with id 123456:"
+    echo "$0 code-review 123456"
+    echo ""
+}
+
+if [[ $# -ne 2 ]]; then
+    usage
+    exit 1
+fi
+
 readonly JOB_NAME="$1"
 readonly JOB_ID="$2"
 
