79b8a5ed529b73a9c882814bb3a19230e5c322c3
Gennadij Yatskov <vokstay@gmx.de>
2021-02-17 17:58:42 +0100

    [gerrit] Improve get-review-comments
    Retrieve all comments from all patchsets.

--- gerrit/get-review-comments.sh
+++ gerrit/get-review-comments.sh
@@ -15,9 +15,29 @@ readonly SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 # Load configuration
 source <(grep = $SCRIPTPATH/config.ini)
 
-readonly GERRIT_QUERY=''
-readonly JQ_FILTER='.currentPatchSet.comments []?'
+readonly JQ_FILTER=$(cat <<- EOM
+    select(has("comments")) | .patchSets | map({number, comments})
+    | map(
+        { number, "comments": [ select(.comments)
+                              | .comments
+                              | map({file, line, message, "reviewer": .reviewer.name })
+                              ]
+        }
+    )
+EOM
+)
 
-readonly CHANGE_ID=$1
+# Retrieve Change-Id from current commit message
+readonly CURRENT_CHANGE_ID="$($SCRIPTPATH/get-change-id.sh)"
 
-ssh -p $GERRIT_PORT $GERRIT_USER@$GERRIT_HOST gerrit query "" --comments --current-patch-set --format json $CHANGE_ID | jq "$JQ_FILTER"
+# Gerrit change ID, defaults to current commit
+readonly CHANGE_ID="${1:-$CURRENT_CHANGE_ID}"
+readonly GERRIT_QUERY="${CHANGE_ID}"
+
+# TODO: Generalize and use the following script
+#$SCRIPTPATH/get-all-comments-filtered.sh "${GERRIT_QUERY}" | jq "$JQ_FILTER"
+
+$SCRIPTPATH/gerrit-query.sh "${GERRIT_QUERY}" --comments --patch-sets --format json \
+    | jq "${JQ_FILTER}"
+
+#ssh -p $GERRIT_PORT $GERRIT_USER@$GERRIT_HOST gerrit query "" --comments --current-patch-set --format json $CHANGE_ID | jq "$JQ_FILTER"
